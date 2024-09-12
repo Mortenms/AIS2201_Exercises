9ff414de987bc8d7f5310a1f259e8fec569f8343
Mortenms <mortenms@stud.ntnu.no>
2024-09-12 10:42:44 +0200

    Ferdig med excersise 3

---
+++ Ark/DFT.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy import cos, pi, sin, exp
+import matplotlib.pyplot as plt
+
+N = 10
+n = np.arange(N)
+x = cos(2*pi/5*n)
+
+m = np.arange(-5, 6, 1)
+Xm = np.zeros(len(m))
+
