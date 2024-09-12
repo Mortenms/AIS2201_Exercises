3c5163ef3ed339229f41fc832354b4d7e4d69a7f
Mortenms <mortenms@stud.ntnu.no>
2024-09-12 10:47:56 +0200

    Ferdig med excersise 3

--- Ark/DFT.py
+++ Ark/DFT.py
@@ -6,6 +6,10 @@ N = 10
 n = np.arange(N)
 x = cos(2*pi/5*n)
 
-m = np.arange(-5, 6, 1)
-Xm = np.zeros(len(m))
+m_array = np.arange(-5, 6, 1)
+Xm = np.zeros(len(m_array))
 
+for m in m_array:
+    Xm[m] = np.sum(x*np.e**(-2j*pi*m*n/N))
+
+plt.stem(m_array, np.abs(Xm))
